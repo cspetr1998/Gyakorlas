6704f4909057c17ea545627543ee4587f623244d
Csesznok Petronella <cspetra1998@gmail.com>
2020-12-15 21:15:39 +0100

    CSV fájl beolvasása

--- Irfprojektproba/Irfprojektproba/Form1.cs
+++ Irfprojektproba/Irfprojektproba/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Irfprojektproba.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +14,40 @@ namespace Irfprojektproba
 {
     public partial class Form1 : Form
     {
-       
+        private BindingList<Rendeles> adat = new BindingList<Rendeles>();
 
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private BindingList<Rendeles> Beolvas(string csvpath)
+        {
+            BindingList<Rendeles> rendelesek = new BindingList<Rendeles>();
+            try
+            {
+                using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] sor = sr.ReadLine().Split(';');
+
+                        Rendeles r = new Rendeles()
+                        {
+                            TortaNev = sor[0],
+                            Allergen = Convert.ToInt32(sor[1]),
+
+                        };
+                        rendelesek.Add(r);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            return rendelesek;
         }
     }
 }
